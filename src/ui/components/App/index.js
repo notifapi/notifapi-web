391da35f5295900e8353707cffde3fdf30a22efe
gorums <acksecurity@hotmail.com>
2016-10-23 20:34:55 -0400

    Fix test app

--- src/ui/components/App/index.js
+++ src/ui/components/App/index.js
@@ -4,9 +4,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    // static propTypes = {}
+    // static defaultProps = {}
+    // state = {}
+
   render() {
+  const { className, ...props } = this.props;
     return (
-      <div className="App">
+      <div className={classnames('App', className)}>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
