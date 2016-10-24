ef82908edcc3be97301b811195a24efefcdabd1b
gorums <acksecurity@hotmail.com>
2016-10-23 22:25:23 -0400

    Add build folder

--- src/app/index.js
+++ src/app/index.js
@@ -6,8 +6,9 @@ const path = require('path');
 const app = express();
 
 // Setup logger
-//app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
-
+if (process.env.NODE_ENV !== 'production') {
+    app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
+}
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, '../..', 'build')));
 
