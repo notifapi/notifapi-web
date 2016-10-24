c5aced0e1e62a2cada44a95c6103d0297f6ceae1
gorums <acksecurity@hotmail.com>
2016-10-24 02:54:53 -0400

    Add clustering feature

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 'use strict';
 
-const app = require('./src/app');
+var throng = require('throng');
+var os = require('os');
 
-const PORT = process.env.PORT || 9000;
+/** clustering **/
+var WORKERS = int(process.env.CONCURRENCY) || os.cpus().length;
+throng({
+    workers: WORKERS,
+    lifetime: Infinity
+}, start);
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-});
+function start(id) {
+    console.log("Started web worker " + id);
+
+    const app = require('./src/app');
+
+    const PORT = process.env.PORT || 9000;
+
+    app.listen(PORT, () => {
+        console.log(`App listening on port ${PORT}!`);
+    });
+}
+
+function int(str) {
+    if (!str) return 0;
+    return parseInt(str, 10);
+}
