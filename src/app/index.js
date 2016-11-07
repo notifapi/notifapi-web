dae473e3adb029dbca5f7dc3014ef458363a961e
gorums <acksecurity@hotmail.com>
2016-11-07 16:18:57 -0500

    Add body-parser and cookie-parser

--- src/app/index.js
+++ src/app/index.js
@@ -2,16 +2,23 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const routes = require('./routes');
 
 const app = express();
+
+// Serve static assets
+app.use(express.static(path.resolve(__dirname, '../..', 'build')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // Setup logger
 if (process.env.NODE_ENV !== 'production') {
     app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 }
-// Serve static assets
-app.use(express.static(path.resolve(__dirname, '../..', 'build')));
 
 // ExpressJs routes
 app.use('/', routes);
