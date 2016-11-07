45634342da8f58dda572fc8ecd37329827f76e61
gorums <acksecurity@hotmail.com>
2016-11-07 16:56:21 -0500

    Add Link Route component in the Home for example purpose.

--- src/ui/components/Home/Home.view.js
+++ src/ui/components/Home/Home.view.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router'
 
 function HomeView (props) {
     return (
         <div>
-            <p>Hello {props.user.user || 'empty'}</p>
+            <p>Hello <Link to="Login">{props.user.user || 'empty'}</Link></p>
         </div>
     );
 }
