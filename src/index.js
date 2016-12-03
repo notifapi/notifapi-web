9041b81c2618ed6cdc80203038fcef60874d2b38
gorums <acksecurity@hotmail.com>
2016-12-03 17:02:19 -0500

    Implement singup front-end

--- src/index.js
+++ src/index.js
@@ -13,15 +13,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
 /** tracer alert and js erros **/
-import Raven from 'raven-js';
-
-// Tracer the error code and warning on production
-Raven.config(process.env.REACT_APP_RAVEN_URL, {
-    environment: process.env.REACT_APP_ENVIRONMENT
-}).install();
+// import Raven from 'raven-js';
+//
+// // Tracer the error code and warning on production
+// Raven.config(process.env.REACT_APP_RAVEN_URL, {
+//     environment: process.env.REACT_APP_ENVIRONMENT
+// }).install();
 
 const ownStore = store();
-console.log(ownStore);
 ReactDOM.render(
     <Provider store={ownStore}>
         <Router history={browserHistory} routes={Routes} />
--- src/ui/components/SingUp.js
+++ src/ui/components/SingUp.js
@@ -6,9 +6,29 @@ export default class SingUp extends Component {
 
     render() {
         return(
-            <h1>
-                Sing Up Here!!!
-            </h1>
+            <div>
+                <div className="singup singup-padding">
+                    <h2>
+                        Start handler notification for free with our restFULL API
+                    </h2>
+                    <p>
+                        Sing up now or scroll down for more details
+                    </p>
+                </div>
+
+                <form className="form-inline text-center">
+                    <div className="form-group singup-form">
+                        <input type="text" className="form-control" placeholder="Username"></input>
+                    </div>
+                    <div className="form-group singup-form">
+                        <input type="email" className="form-control"  placeholder="Email"></input>
+                    </div>
+                    <div className="form-group singup-form">
+                        <input type="password" className="form-control"  placeholder="Password"></input>
+                    </div>
+                    <button type="submit" className="btn btn-success singup-form">Sing Up</button>
+                </form>
+            </div>
         )
     }
 }
